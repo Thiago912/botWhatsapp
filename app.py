32303cc81b3335d5a1fa3c62e9ef6a423e52d4d0
Thiago912 <thiagofernandedz1345@gmail.com>
2025-09-05 20:03:47 -0300

    Actualizo app.py y agrego catálogo de espejos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-import openai
+import pandas as pd
+import time
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
@@ -12,16 +13,24 @@ app = Flask(__name__)
 # Configura la API key de OpenAI desde .env
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
+df = pd.read_excel("espejos.xlsx")
+catalogo = "\n".join([f"{row['modelo']}: ${row['precio']}" for _, row in df.iterrows()])
+
 @app.route("/webhook", methods=["POST"])
 def whatsapp_reply():
     incoming_msg = request.values.get("Body", "").strip()
     
     # Llamar a GPT
     response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": incoming_msg}]
+    model="gpt-5-mini",
+    
+    messages=[
+        {"role": "system", "content": f"Sos un vendedor de espejos. Catálogo:\n{catalogo}. Respondé con simpatía, claridad y brevedad. No uses más de 3 líneas."},
+        {"role": "user", "content": incoming_msg}]
 )
     reply = response.choices[0].message.content
+
+    time.sleep(5)
 
     # Responder por WhatsApp
     twilio_response = MessagingResponse()
